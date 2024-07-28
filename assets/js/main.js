6d7696d55cbcc0daac579aaf14ae3cda6a1ab532
Riptide-11 <69417017+Riptide-11@users.noreply.github.com>
2024-07-28 11:09:04 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,266 @@
+/***************************************************
+==================== JS INDEX ======================
+****************************************************
+01. PreLoader Js
+
+
+****************************************************/
+
+(function ($) {
+	"use strict";
+
+	var windowOn = $(window);
+	////////////////////////////////////////////////////
+	// 01. PreLoader Js
+	windowOn.on('load',function() {
+		$("#loading").fadeOut(500);
+	});
+
+	////////////////////////////////////////////////////
+	// 02. Mobile Menu Js
+	$('#mobile-menu').meanmenu({
+		meanMenuContainer: '.mobile-menu',
+		meanScreenWidth: "991",
+		meanExpand: ['<i class="fal fa-plus"></i>'],
+	});
+
+	////////////////////////////////////////////////////
+	// 03. Sidebar Js
+	$(".offcanvas-toggle-btn").on("click", function () {
+		$(".offcanvas__area").addClass("opened");
+		$(".body-overlay").addClass("opened");
+	});
+	$(".offcanvas__close-btn").on("click", function () {
+		$(".offcanvas__area").removeClass("opened");
+		$(".body-overlay").removeClass("opened");
+	});
+
+
+	////////////////////////////////////////////////////
+	// 04. Body overlay Js
+	$(".body-overlay").on("click", function () {
+		$(".offcanvas__area").removeClass("opened");
+		$(".body-overlay").removeClass("opened");
+	});
+
+
+	////////////////////////////////////////////////////
+	// 05. Search Js
+	$(".search-toggle").on("click", function () {
+		$(".search__area").addClass("opened");
+	});
+	$(".search-close-btn").on("click", function () {
+		$(".search__area").removeClass("opened");
+	});
+
+
+	////////////////////////////////////////////////////
+	// 06. Sticky Header Js
+	windowOn.on('scroll', function () {
+		var scroll = $(window).scrollTop();
+		if (scroll < 100) {
+			$("nav").removeClass("bg-white");
+		} else {
+			$("nav").addClass("bg-white");
+		}
+	});
+
+
+	////////////////////////////////////////////////////
+	// 07. Data CSS Js
+	$("[data-background").each(function () {
+		$(this).css("background-image", "url( " + $(this).attr("data-background") + "  )");
+	});
+	
+	$("[data-width]").each(function () {
+		$(this).css("width", $(this).attr("data-width"));
+	});
+
+	$("[data-bg-color]").each(function () {
+        $(this).css("background-color", $(this).attr("data-bg-color"));
+    });
+
+	////////////////////////////////////////////////////
+	// 07. Nice Select Js
+	$('select').niceSelect();
+
+	////////////////////////////////////////////////////
+	// 08. slider__active Slider Js
+	if (jQuery(".slider__active").length > 0) {
+		let sliderActive1 = ".slider__active";
+		let sliderInit1 = new Swiper(sliderActive1, {
+			// Optional parameters
+			slidesPerView: 1,
+			slidesPerColumn: 1,
+			paginationClickable: true,
+			loop: true,
+			effect: 'fade',
+
+			autoplay: {
+				delay: 5000,
+			},
+
+			// If we need pagination
+			pagination: {
+				el: ".ain-slider-paginations",
+				// dynamicBullets: true,
+				clickable: false,
+			},
+
+			// Navigation arrows
+			navigation: {
+				nextEl: ".slider-button-next",
+				prevEl: ".slider-button-prev",
+			},
+
+			a11y: false,
+		});
+
+		function animated_swiper(selector, init) {
+			let animated = function animated() {
+				$(selector + " [data-animation]").each(function () {
+					let anim = $(this).data("animation");
+					let delay = $(this).data("delay");
+					let duration = $(this).data("duration");
+
+					$(this)
+						.removeClass("anim" + anim)
+						.addClass(anim + " animated")
+						.css({
+							webkitAnimationDelay: delay,
+							animationDelay: delay,
+							webkitAnimationDuration: duration,
+							animationDuration: duration,
+						})
+						.one(
+							"webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+							function () {
+								$(this).removeClass(anim + " animated");
+							}
+						);
+				});
+			};
+			animated();
+			// Make animated when slide change
+			init.on("slideChange", function () {
+				$(sliderActive1 + " [data-animation]").removeClass("animated");
+			});
+			init.on("slideChange", animated);
+		}
+
+		animated_swiper(sliderActive1, sliderInit1);
+	}
+
+
+	var sliderr = new Swiper('.active', {
+		slidesPerView: 1,
+		spaceBetween: 30,
+		loop: true,
+		pagination: {
+			el: ".testimonial-pagination",
+			clickable: true,
+			renderBullet: function (index, className) {
+			  return '<span class="' + className + '">' + '<button>'+(index + 1)+'</button>' + "</span>";
+			},
+		},
+		breakpoints: {
+			'1200': {
+				slidesPerView: 3,
+			},
+			'992': {
+				slidesPerView: 2,
+			},
+			'768': {
+				slidesPerView: 2,
+			},
+			'576': {
+				slidesPerView: 1,
+			},
+			'0': {
+				slidesPerView: 1,
+			},
+		},
+	});
+
+	///////////////////////////////////////////////////
+	// 13. Masonary Js
+	$(".testimonial__active").owlCarousel({
+		//add owl carousel in activation class
+		loop: true,
+		margin: 30,
+		items: 4,
+		navText: ['<button class="nav-left"><i class="far fa-angle-left"></i></button>', '<button class="nav-right"><i class="far fa-angle-right"></i></button>'],
+		nav: false,
+		dots: true,
+		slideTransition: 'fadeOutLeft',
+		smartSpeed: 300,
+		responsive: {
+			0: {
+				items: 1
+			},
+			767: {
+				items: 2
+			},
+			992: {
+				items: 3
+			},
+			1200: {
+				items: 3
+			}
+		}
+	});
+
+
+	// course slider
+	var swiper = new Swiper('.course__slider', {
+		spaceBetween: 30,
+		slidesPerView: 2,
+		breakpoints: {  
+			'768': {
+				slidesPerView: 2,
+			},
+			'576': {
+				slidesPerView: 1,
+			},
+			'0': {
+				slidesPerView: 1,
+			},
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+	});
+
+	var postboxSlider = new Swiper('.postbox__slider', {
+		slidesPerView: 1,
+        spaceBetween: 0,
+		loop: true,
+		autoplay: {
+		  delay: 3000,
+		},
+		// Navigation arrows
+		navigation: {
+			nextEl: ".postbox-slider-button-next",
+			prevEl: ".postbox-slider-button-prev",
+		},
+		breakpoints: {  
+			'1200': {
+				slidesPerView: 1,
+			},
+			'992': {
+				slidesPerView: 1,
+			},
+			'768': {
+				slidesPerView: 1,
+			},
+			'576': {
+				slidesPerView: 1,
+			},
+			'0': {
+				slidesPerView: 1,
+			},
+		},
+	});
+
+})(jQuery);
